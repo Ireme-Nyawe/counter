489a905a350b461bb2ec8b100045edba5944110b
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-13 21:23:50 +0200

    set up counter structure

---
+++ src/components/Counter.jsx
@@ -0,0 +1,11 @@
+export default function Counter() {
+  return (
+    <div className="counter-container">
+      <div className="counted">0</div>
+      <div className="counter-sign">
+        <span className="sign">-</span>
+        <span className="sign">+</span>
+      </div>
+    </div>
+  );
+}
