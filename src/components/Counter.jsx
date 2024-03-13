ba73fca23359f180d43f56f2a4f34c39e465febd
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-13 21:41:37 +0200

    apply callback to use updater for best paractices

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,26 +2,20 @@ import { useState } from "react";
 
 export default function Counter() {
   const [counts, setCount] = useState(0);
-  function handleMinus(){
-    setCount(counts-1);
+  function handleMinus() {
+    setCount((counted) => counted - 1);
   }
-  function handlePlus(){
-    setCount(counts+1);
+  function handlePlus() {
+    setCount((counted) => counted + 1);
   }
   return (
     <div className="counter-container">
       <div className="counted">{counts}</div>
       <div className="counter-sign">
-        <span
-          className="sign"
-          onClick={handleMinus}
-        >
+        <span className="sign" onClick={handleMinus}>
           -
         </span>
-        <span
-          className="sign"
-          onClick={handlePlus}
-        >
+        <span className="sign" onClick={handlePlus}>
           +
         </span>
       </div>
