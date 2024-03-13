c03acbd133c35036cba2af17adb52f852db17d4a
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-13 21:32:58 +0200

    use Hooks to manage state;UseState()

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+
 export default function Counter() {
+  const [counts, setCount] = useState(0);
+  function handleMinus(){
+    setCount(counts-1);
+  }
+  function handlePlus(){
+    setCount(counts+1);
+  }
   return (
     <div className="counter-container">
-      <div className="counted">0</div>
+      <div className="counted">{counts}</div>
       <div className="counter-sign">
-        <span className="sign">-</span>
-        <span className="sign">+</span>
+        <span
+          className="sign"
+          onClick={handleMinus}
+        >
+          -
+        </span>
+        <span
+          className="sign"
+          onClick={handlePlus}
+        >
+          +
+        </span>
       </div>
     </div>
   );
